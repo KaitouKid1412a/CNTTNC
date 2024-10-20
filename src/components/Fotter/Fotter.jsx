48430b0a4f0e7d1266a9caf93e2159a06888c950
namprolop87@gmail.com <namprolop87@gmail.com>
2024-10-20 10:22:19 +0700

    Initial commit

--- src/components/Fotter/Fotter.jsx
+++ src/components/Fotter/Fotter.jsx
@@ -13,31 +13,31 @@ export default function Fotter() {
             </div>
             <div className="boxft">
                 <div className="title">Quick Link</div>
+                <div className="call">About</div>
+                <div className="adress">Contact</div>
+                <div className="adress">Pricing</div>
+                <div className="adress">Blog</div>
+            </div>
+            <div className="boxft">
+                <div className="title">Candidate</div>
                 <div className="call">Browse Jobs</div>
                 <div className="adress">Browse Employers</div>
                 <div className="adress">Candidate Dashboard</div>
                 <div className="adress">Saved Jobs</div>
             </div>
             <div className="boxft">
-                <div className="title">Quick Link</div>
-                <div className="call">Browse Jobs</div>
-                <div className="adress">Browse Employers</div>
-                <div className="adress">Candidate Dashboard</div>
-                <div className="adress">Saved Jobs</div>
+                <div className="title">Employers</div>
+                <div className="call">Post a Job</div>
+                <div className="adress">Browse Candidates</div>
+                <div className="adress">Employers Dashboard</div>
+                <div className="adress">Applications</div>
             </div>
             <div className="boxft">
-                <div className="title">Quick Link</div>
-                <div className="call">Browse Jobs</div>
-                <div className="adress">Browse Employers</div>
-                <div className="adress">Candidate Dashboard</div>
-                <div className="adress">Saved Jobs</div>
-            </div>
-            <div className="boxft">
-                <div className="title">Quick Link</div>
-                <div className="call">Browse Jobs</div>
-                <div className="adress">Browse Employers</div>
-                <div className="adress">Candidate Dashboard</div>
-                <div className="adress">Saved Jobs</div>
+                <div className="title">Support</div>
+                <div className="call">Faqs</div>
+                <div className="adress">Privacy Policy</div>
+                <div className="adress">Terms & Conditions</div>
+                <div className="adress"></div>
             </div>
             
         </div>
